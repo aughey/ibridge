aac1385c83c73eacf9a1db4987e8a11e7769c8c3
John Aughey <jha@aughey.com>
2012-10-09 22:25:18 -0500

    Building the start of a socks client.  Mostly works

--- server.js
+++ server.js
@@ -1,40 +1,58 @@
 var common = require("./common");
 var net = require('net');
-var argv = require('optimist');
+var argv = require('optimist').boolean('o').argv;
 
 var server = common.newServer(argv['p'] || 3001);
+var openconnection = argv.o || false
+
+if(openconnection) {
+	console.log("Warning: running with openconnection.  Possibly problematic.")
+}
 
 server.newEndpoint('open', function(open_data) {
-    var c = net.connect(22, "localhost", function() {
-      console.log("Connecting to local ssh server");
+	var host = open_data['host'] || "localhost"
+	var port = open_data['port'] || 22
+	if(!openconnection) {
+		host = 'localhost'
+		port = 22;
+	}
+	var c = net.connect(port, host, function() {
 
-      function writeData(d) {
-        c.write(new Buffer(d.d, 'binary'));
-      }
-      var ep = server.newEndpoint(null, writeData, function() {
-        c.end();
-      });
+		function writeData(d) {
+			c.write(new Buffer(d.d, 'binary'));
+		}
+		var ep = server.newEndpoint(null, writeData, function() {
+			c.end();
+		});
 
-      c.on('data', function(d) {
-        server.sendMessage({
-          c: open_data.i,
-          d: d.toString('binary')
-        });
-      })
-      c.on('end', function() {
-        ep.close();
-      })
-      // Let the remote side know this endpoint
-      server.sendMessage({
-        c: open_data.i,
-        i: ep.id
-      });
-    })
-  });
+		c.on('data', function(d) {
+			server.sendMessage({
+				c: open_data.i,
+				d: d.toString('binary')
+			});
+		})
+		c.on('close', function() {
+			server.sendMessage({
+				c: 'x',
+				i: open_data.i
+			});
+			ep.close();
+		})
+		// Let the remote side know this endpoint
+		server.sendMessage({
+			c: open_data.i,
+			i: ep.id
+		});
+	})
+	c.on('error',function(err) {
+		console.log("Error with connection to " + host + ":" + port);
+		console.log(err);
+	})
+});
 
 setInterval(function() {
-  // Ping the other side.
-  server.sendMessage({
-    c: 'p'
-  })
+	// Ping the other side.
+	server.sendMessage({
+		c: 'p'
+	})
 }, 1000);
