d53420814527c380de37197cc2f2e8dc790c10ab
John Aughey <jha@aughey.com>
2012-10-10 15:42:10 -0500

    Some UI changes

--- ui/html/ui.js
+++ ui/html/ui.js
@@ -1,13 +1,11 @@
-function ui_event(name,data) {
-	if(name != 'address')
-		return;
+function ui_event(name, data) {
+	if(name != 'address') return;
 	//$('#content').append("<div>got ui event " + name + ": " + JSON.stringify(data) + "</div>");
-	if(data.interface == 'en1') {
-		$('#qrcode').qrcode("http://" + data.addresses[data.addresses.length-1] + ":3000");
-	}
+	var url = "http://" + data.addresses[data.addresses.length - 1] + ":3000";
+	$('#qrcode').qrcode(url);
+	$('#content').html("<p>Go to " + url + " on your smart phone's web browser.  Or simply scan the QRCode below.</p>")
 }
 
-function init() {
-}
+function init() {}
 
 $(init);
