29ef7437b487f34b0c1531bd6f1b0e5dcf8293d6
John Aughey <jha@aughey.com>
2012-10-09 20:18:21 -0500

    Adding uglify to the compile

--- compile.sh
+++ compile.sh
@@ -1,9 +1,5 @@
-level="SIMPLE"
-#level="ADVANCED"
 for f in *.js; do 
 	echo "Compiling $f"
-	grep -v 'console' < $f > compiled/foo
-	curl -s -d compilation_level=${level}_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@compiled/foo" http://closure-compiler.appspot.com/compile > compiled/$f
-	rm -f compiled/foo
+	grep -v 'console' < $f | ./node_modules/uglify-js/bin/uglifyjs > compiled/$f
 done
 
